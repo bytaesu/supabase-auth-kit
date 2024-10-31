47af1b5bb1a9a475c7e12de234335bb0e3757fa7
Taesu <bytaesu@gmail.com>
2024-10-31 15:38:30 +0900

    refactor: improve code readability and structure

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -18,44 +18,40 @@ const ForgotPasswordPage = () => {
     exitVerificationStage,
   } = useVerificationStage();
 
+  // Show verification stage component if user is in verification stage
+  if (isVerificationStage) {
+    return (
+      <VerificationStage
+        emailOtpType="recovery"
+        userEmail={userEmail}
+        handleExit={exitVerificationStage}
+      />
+    );
+  }
+
+  // Show forgot password form if user is not in verification stage
   return (
     <div>
-      {isVerificationStage ? (
-        // Show verification stage component if user is in verification stage
-        <VerificationStage
-          emailOtpType="recovery"
-          userEmail={userEmail}
-          handleExit={exitVerificationStage}
-        />
-      ) : (
-        // Show forgot password form if user is not in verification stage
-        <div>
-          <CardHeader>
-            <h1 className="text-center text-2xl font-semibold">
-              Reset password
-            </h1>
-            <p className="text-muted-foreground text-sm font-normal">
-              Enter the email address associated with your account and we will
-              send you a code to reset your password.
-            </p>
-          </CardHeader>
+      <CardHeader>
+        <h1 className="text-center text-2xl font-semibold">Reset password</h1>
+        <p className="text-muted-foreground text-sm font-normal">
+          Enter the email address associated with your account and we will send
+          you a code to reset your password.
+        </p>
+      </CardHeader>
 
-          <CardContent>
-            <ForgotPasswordForm
-              enterVerificationStage={enterVerificationStage}
-            />
-          </CardContent>
+      <CardContent>
+        <ForgotPasswordForm enterVerificationStage={enterVerificationStage} />
+      </CardContent>
 
-          <CardFooter className="p-4 border-t bg-muted flex justify-center items-center">
-            <Link href={AUTH_ROUTES.Public.SignIn}>
-              <div className="flex text-blue-500 justify-center items-center">
-                <ChevronLeft size={20} />
-                <p className="text-sm font-medium">Back to sign in</p>
-              </div>
-            </Link>
-          </CardFooter>
-        </div>
-      )}
+      <CardFooter className="p-4 border-t bg-muted flex justify-center items-center">
+        <Link href={AUTH_ROUTES.Public.SignIn}>
+          <div className="flex text-blue-500 justify-center items-center">
+            <ChevronLeft size={20} />
+            <p className="text-sm font-medium">Back to sign in</p>
+          </div>
+        </Link>
+      </CardFooter>
     </div>
   );
 };
